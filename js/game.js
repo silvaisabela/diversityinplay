f84b7217002e5465d5f039919a33f3298e6c0d96
silvaisabela <isabelai27@hotmail.com>
2019-07-08 16:46:05 -0300

    Criar mecanica de Level
    - Criar score e level

--- js/game.js
+++ js/game.js
@@ -1,32 +1,79 @@
 // Uma função que configura as ações dos jogos/ Quando é carregado uma vez só no game
 let avatar
 let obstacles = []
+let level = 1
+let score = 0
 
-function setup(){
-    createCanvas(600, 450)
-    avatar = new Avatar()
+const levelGoal = {
+    1: 50,
+    2: 300,
+    3: 450,
+    4: 600
 }
 
-function keyPressed(){
-    if (key == ' '){
+
+function setup() {
+    createCanvas(600, 450)
+    avatar = new Avatar()
+    obstacles.push(new Obstacle())
+}
+/* Aqui estou criando o perimetro do jogo e do avatar */
+
+
+function keyPressed() {
+    if (key == ' ') {
         avatar.jump()
     }
 }
 
-// Issso é uma função onde "draw"(desenhar) vai dar uma cor ao fundo. Aqui ficara as coisas que sempre será carregada.
-function draw(){
-    if(random(1) < 0.01){
-        obstacles.push(new Obstacle())
+/* Quando a barra de espaço é prensionada, essa função é ativada. e verifico se a tecla precionada é o espaço se for eu pulo se não por eu não faço nada */
+
+
+/*  O draw vai ser chamado a todo momento para desenhar o objeto na tela */
+function draw() {
+
+    if (random(1) < 0.05 && obstacles.length) {
+        if (obstacles[obstacles.length - 1].x < width - width / 3) {
+            obstacles.push(new Obstacle())
+        }
     }
+
+    /* if(random(1) < 0.009) : Calcula a frequencia com que os objetos apareceram na tela 
+    obstacles.push(new Obstacle()) : Adiciona um obstaculo na lista */
 
     background(220)
     avatar.show()
     avatar.move()
 
-    for (let o of obstacles){
+    /* Aqui estamos definindo a cor de fundo e chamando as funções do avatar de mover (move) e mostrar (show)  */
+
+    for (let i = 0; i < obstacles.length; i++) {
+        let o = obstacles[i];
         o.move()
         o.show()
+        if (avatar.hits(o)) {
+            noLoop()
+        }
+        if (o.x == 0) {
+            obstacles.splice(i, 1)
+            score = score + 10
+            console.log(score)
+        }
+    }
+
+    /*for (let o of obstacles){
+        o.move()
+        o.show()
+    ---> Pegando cada obejto que está na lista, mostrando na tela e movendo eles. 
+
+    if(avatar.hits(o)){
+            noLoop()
+        }
+    ---> Essa função está verificando se o avatar bateu em cada obstaculo adicionado na lista.
+    */
+
+    if (levelGoal[level] === score){
+        score = 0
+        console.log('Passou de nivel')
     }
 }
-
-
