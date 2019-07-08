ed4824547468747fbd36a40cf6dcf84aef7ba633
silvaisabela <isabelai27@hotmail.com>
2019-07-08 16:46:05 -0300

    Criar mecanica de Level
    - Criar score e level

--- js/avatar.js
+++ js/avatar.js
@@ -1,25 +1,54 @@
 class Avatar {
-    constructor(){
+    constructor() {
         this.size = 50
         this.x = this.size
         this.y = height - this.size
         this.forceY = 0
-        this.gravity = 2
+        this.gravity = 1
+    }
+    /* Constructor --->
+      Aqui estamos definindo onde ele será criado e qual tamanho vai ter
+ - this.size: Está adicionando um tamanho ao objeto
+ - this.x = width :Estou informanso em que posição da tela no eixo x (horizontal) o objeto vai estar.
+ - this.y = height - this.size : Estou informanso em que posição da tela no eixo y (vertical) o objeto vai estar. 
+ -this.forceY: 0  : Estamos definindo a força que jogo o quadrado para cima
+  -this.gravity: 1  : Estamos definindo a força que jogo o quadrado para baixo */
+
+
+    isInitialPosition() {
+        return this.y == height - this.size
+    }
+    jump() {
+        if (this.isInitialPosition()) {
+            this.forceY = -20
+        }
     }
 
-    jump(){
-        this.forceY = -25
+    /* Jump** -->
+    Se o avatar estiver na posição inicial ele pula se não ele não faz nada */
+
+    hits(obstacle) {
+        return collideRectRect(this.x, this.y, this.size, this.size, obstacle.x, obstacle.y, obstacle.size, obstacle.size)
     }
 
-    move (){
+
+    /* Essa função está verificando se o avatar bateu no obstaculo.*/ 
+
+    move() {
         this.y += this.forceY
         this.forceY += this.gravity
         this.y = constrain(this.y, 0, height - this.size)
     }
 
-    show(){
+
+    /* Move** 
+    - Ele faz com que o objeto mova de baixo para cima, ou seja, pega o y aciona -20 e diminui o valor da força com a gravidade.
+    - this.y = constrain(this.y, 0, height - this.size) impede com que a gravidade empurre o quadrado para fora da tela.
+    */
+
+    show() {
         rect(this.x, this.y, this.size, this.size)
     }
-
+    /* O Show** -->
+ - rect : cria o quadrado e apresenta ele na tela */
 }
-
--- js/obstacle.js
+++ js/obstacle.js
@@ -3,14 +3,28 @@ class Obstacle {
         this.size = 50
         this.x = width
         this.y = height - this.size
-
     }
+    /*  **Constructor**  --> 
+    Aqui estamos definindo onde ele será criado e qual tamanho vai ter
+ - this.size: Está adicionando um tamanho ao objeto
+ - this.x = width :Estou informanso em que posição da tela no eixo x (horizontal) o objeto vai estar.
+ - this.y = height - this.size : Estou informanso em que posição da tela no eixo y (vertical) o objeto vai estar. */
 
-    move() {
-        this.x -= 3
-    }
+    move(velocity) {
+        if (velocity == null)
+        velocity = 5
+        this.x = this.x - velocity
+        // this.x -= velocity
+     }
+
+    /* A **Move** -->
+ - if (velocity == null) = ele verifica se a velocidade  não existe, se não existir o valor da velocidade é = 5 (igual a cinco) que está definido em velocity = 5 s
+ - this.x -= velocity <--- "abreviado" ou  this.x = this.x - velocity  = aqui ele está pegando o x e diminuindo seu valor a cada vez que o move é chamada. Movendo o objeto da direita da esquerda */
+
 
     show() {
         rect(this.x, this.y, this.size, this.size)
     }
+    /* O Show** -->
+ - rect : cria o quadrado e apresenta ele na tela */
 }
