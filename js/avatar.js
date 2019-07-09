2909de6dd0309d1b2167a938a7cce8a898680498
silvaisabela <isabelai27@hotmail.com>
2019-07-08 23:32:39 -0300

    Criar documentação do jogo

--- js/avatar.js
+++ js/avatar.js
@@ -6,62 +6,62 @@ class Avatar {
         this.forceY = 0
         this.gravity = 1
         this.life = 3
-
     }
-    /* Constructor --->
-      Aqui estamos definindo onde ele será criado e qual tamanho vai ter
- - this.size: Está adicionando um tamanho ao objeto
- - this.x = width :Estou informanso em que posição da tela no eixo x (horizontal) o objeto vai estar.
- - this.y = height - this.size : Estou informanso em que posição da tela no eixo y (vertical) o objeto vai estar. 
- -this.forceY: 0  : Estamos definindo a força que jogo o quadrado para cima
-  -this.gravity: 1  : Estamos definindo a força que jogo o quadrado para baixo */
 
-
+    /**
+     * Verifica se o avatar está mesma posição que foi criado
+     */
     isInitialPosition() {
         return this.y == height - this.size
     }
 
+    /**
+     * O jump faz o avatar pular
+     */
     jump() {
         if (this.isInitialPosition()) {
             this.forceY = -20
         }
     }
-    /* Jump** -->
-    Se o avatar estiver na posição inicial ele pula se não ele não faz nada */
-
+    
+    /**
+     * Verifica se o avatar bateu em algum obstaculo
+     * @param {Obstacle} obstacle É um elemento que pode ser atingindo pelo avatar
+     */
     hits(obstacle) {
         return collideRectRect(this.x, this.y, this.size, this.size, obstacle.x, obstacle.y, obstacle.size, obstacle.size)
     }
 
-
-    /* Essa função está verificando se o avatar bateu no obstaculo.*/
-
+    /**
+     * Move o avatar de cima para baixo e evita que ele saia da tela 
+     */
     move() {
         this.y += this.forceY
         this.forceY += this.gravity
         this.y = constrain(this.y, 0, height - this.size)
     }
 
-
-    /* Move** 
-    - Ele faz com que o objeto mova de baixo para cima, ou seja, pega o y aciona -20 e diminui o valor da força com a gravidade.
-    - this.y = constrain(this.y, 0, height - this.size) impede com que a gravidade empurre o quadrado para fora da tela.
-    */
-
+    /**
+     * Mostra o avatar e a vida
+     */
     show() {
         rect(this.x, this.y, this.size, this.size)
         textSize(15)
         text(`Life: ${this.life}`, 10, 20)
     }
-    /* O Show** -->
- - rect : cria o quadrado e apresenta ele na tela */
 
+    /**
+     * Diminui o valor da vida caso ela seja maior que 0
+     */
     lostLife() {
         if (this.life > 0) {
             this.life = this.life - 1
         }
     }
 
+    /**
+     * Verifica se o avatar perdeu todas as vidas
+     */
     isDead(){
         return this.life === 0 
     }
--- js/obstacle.js
+++ js/obstacle.js
@@ -5,30 +5,24 @@ class Obstacle {
         this.y = height - this.size
         this.hited = false
     }
-    /*  **Constructor**  --> 
-    Aqui estamos definindo onde ele será criado e qual tamanho vai ter
- - this.size: Está adicionando um tamanho ao objeto
- - this.x = width :Estou informanso em que posição da tela no eixo x (horizontal) o objeto vai estar.
- - this.y = height - this.size : Estou informanso em que posição da tela no eixo y (vertical) o objeto vai estar. */
 
-    move(velocity) {
-        if (velocity == null)
-            velocity = 5
-        this.x = this.x - velocity
-        // this.x -= velocity
+    /**
+     * Move o obstaculo da direita para esquerda
+     */
+    move() {
+        this.x = this.x - 5
     }
 
-    /* A **Move** -->
- - if (velocity == null) = ele verifica se a velocidade  não existe, se não existir o valor da velocidade é = 5 (igual a cinco) que está definido em velocity = 5 s
- - this.x -= velocity <--- "abreviado" ou  this.x = this.x - velocity  = aqui ele está pegando o x e diminuindo seu valor a cada vez que o move é chamada. Movendo o objeto da direita da esquerda */
-
-
+    /**
+     * Mostra o obstaculo na tela
+     */
     show() {
         rect(this.x, this.y, this.size, this.size)
     }
-    /* O Show** -->
- - rect : cria o quadrado e apresenta ele na tela */
 
+    /**
+     * Retorna verdadeiro se o obstaculo sair da tela pelo lado esquerdo
+     */
     isHide() {
         return this.x == 0
     }
